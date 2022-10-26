d0f17015b9c40fca91ed4045ba86282dc5734a43
hassan ibrahim <hassan.ibrahim@scytalelabs.com>
2022-10-26 13:28:25 +0500

    Removed Gauge controller and minter js clients

--- JsClients/GAUGECONTROLLER/src/index.ts
+++
@@ -1,9 +0,0 @@
-import GaugeControllerClient from "./gaugeController";
-import * as utils from "./utils";
-import * as constants from "./constants";
-
-export {
-    GaugeControllerClient,
-    utils,
-    constants
-};
